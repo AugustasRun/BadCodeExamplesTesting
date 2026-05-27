f812d8a61422ccb5ef0d41d072887833ff5755a1
AugustasRun <53519808+AugustasRun@users.noreply.github.com>
2026-05-27 07:56:44 +0300

    add

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    internal sealed class Program
+    {
+        static async Task Main(string[] args)
+        {
+            UnsafeQuery("test", "test", "test");
+        }
+
+        public static object UnsafeQuery(string connection, string name, string password)
+        {
+            SqlConnection someConnection = new SqlConnection(connection);
+            using SqlCommand someCommand = new SqlCommand();
+            someCommand.Connection = someConnection;
+
+            someCommand.CommandText = "SELECT AccountNumber FROM Users " +
+               "WHERE Username='" + name +
+               "' AND Password='" + password + "'";
+
+            someConnection.Open();
+            object accountNumber = someCommand.ExecuteScalar();
+            someConnection.Close();
+            return accountNumber;
+        }
+    }
+}
